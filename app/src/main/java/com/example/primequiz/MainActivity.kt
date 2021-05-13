0f51c091d86f84d26d4cf44bc4f68e4f2d8b7ad7
ogoogo <ogosae.1018173@icloud.com>
2021-05-14 00:10:38 +0900

    素数アプリ完成

---
+++ app/src/main/java/com/example/primequiz/MainActivity.kt
@@ -0,0 +1,108 @@
+package com.example.primequiz
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val QUESTION_COUNT = 10
+    }
+    var random:Random = Random
+    val questions :IntArray= IntArray(QUESTION_COUNT)
+    var point : Int = 0
+    var answerCount : Int = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        for (i in 0 until QUESTION_COUNT){
+            val number = random.nextInt(1000)
+            Log.d("Number", "Question"+number.toString())
+            questions[i]=number
+        }
+        point = 0
+        answerCount = 0
+
+        numberText.text=questions[answerCount].toString()+""
+        numberText.setTextColor(Color.BLACK)
+    }
+
+    fun maru(v: View){
+        var answer = true
+        val number = questions[answerCount]
+
+        for(i in 2 until number){
+            if (number%i == 0){
+                answer = false
+                break
+            }
+        }
+        if (answer){
+            Toast.makeText(this,"正解",Toast.LENGTH_SHORT).show()
+            point++
+        }else{
+            Toast.makeText(this,"不正解",Toast.LENGTH_SHORT).show()
+        }
+        if (answer){
+            Log.d("maru","正解"+point.toString())
+        }else{
+            Log.d("batsu", "不正解")
+        }
+        answerCount++
+
+        if (answerCount== QUESTION_COUNT){
+            numberText.text=point.toString()+"点"
+            numberText.setTextColor(Color.RED)
+            point=0
+            answerCount=0
+        }else{
+            numberText.text=questions[answerCount].toString()+""
+            numberText.setTextColor(Color.BLACK)
+        }
+
+    }
+    fun batsu(v: View){
+        var answer = false
+        val number = questions[answerCount]
+
+        for(i in 2 until number){
+            if (number%i == 0){
+                answer = true
+                break
+            }
+        }
+        if (answer){
+            Toast.makeText(this,"正解",Toast.LENGTH_SHORT).show()
+            point++
+        }else{
+            Toast.makeText(this,"不正解",Toast.LENGTH_SHORT).show()
+        }
+        if (answer){
+            Log.d("maru","正解"+point.toString())
+        }else{
+            Log.d("batsu", "不正解")
+        }
+        answerCount++
+
+        if (answerCount== QUESTION_COUNT){
+            numberText.text=point.toString()+"点"
+            numberText.setTextColor(Color.RED)
+            point=0
+            answerCount=0
+        }else{
+            numberText.text=questions[answerCount].toString()+""
+            numberText.setTextColor(Color.BLACK)
+        }
+
+    }
+
+}
